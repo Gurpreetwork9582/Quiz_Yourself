bc60c3c8704e9e6e50042548b3cd383b64d33fe1
Gurpreetwork9582 <gurpreet.singh@bmmca.internal>
2026-03-04 12:20:54 -0400

    See the data whenadded new questions

--- pages/Manual_Que.py
+++ pages/Manual_Que.py
@@ -4,6 +4,10 @@ import pandas as pd
 
 st.title("Create Quiz Question")
 
+#Initializing the Session to store the data in rerun
+if 'key' not in st.session_state:
+    st.session_state.key = []
+
 # User inputs
 question_text = st.text_input("Enter Question")
 option_a = st.text_input("Option A")
@@ -11,19 +15,25 @@ option_b = st.text_input("Option B")
 option_c = st.text_input("Option C")
 option_d = st.text_input("Option D")
 
-answer = st.selectbox(
-    "Correct Answer",
-    ["A", "B", "C", "D"]
-)
+answer = st.selectbox("Correct Answer",["A", "B", "C", "D"])
 
 if st.button("Save Question"):
-    question = pd.DataFrame([{
+
+    question = {
         "question": question_text,
         "option A": option_a,
         "option B": option_b,
         "option C": option_c,
         "option D": option_d,
-        "answer": answer }])
+        "answer": answer 
+        }
+    
+    st.session_state.key.append(question)
     st.success("Question saved!")
-    st.write(question)
+    
+
+if st.session_state.key:
+    df = pd.DataFrame(st.session_state.key)
+    st.dataframe(df)
+
     
